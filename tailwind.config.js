401da9c6c9bc205c48b884224c1223d72617fed2
Aditya Rai <adityarai0804@gmail.com>
2025-05-09 17:46:58 +0530

    Final updates before deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    darkMode: 'class',
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+  darkMode: 'class', // or 'media'
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
